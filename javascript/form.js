89c2d6f1ed6e5ad5a10af84afa6e911f91e5ef49
Amairani <alopez21@hawk.iit.edu>
2018-09-27 00:37:33 -0500

    Added javascript alert correctly. Went ahead and added it in the index.

--- javascript/form.js
+++ javascript/form.js
@@ -1,13 +1,4 @@
-function confirmSubmit(evt){
-  var fullname = document.getElementById("firstname");
-  var span = document.getElementsByClassName("close")[0];
-  var modal = document.getElementById('confirmBox');
-  var btn = document.getElementById('submit');
-
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  alert("Thanks " + firstname.value + " for joining us");
+function confirmSubmit(){
+  var fullname = document.getElementById("name");
+  alert("Thanks " + fullname.value + " for signing up!");
+}
